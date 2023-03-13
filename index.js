8bf9382156504ac7b80063b45742cc4a07ed5f34
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-03-13 12:08:24 +0100

    fix bug usedElements size

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const bubble = document.getElementById('bubble')
 const bubbleContent = document.getElementById('content')
 
+let usedElements = new Set();
 import("./assets/data/data.js")
     .then(module => {
         const dataChuckNorris = module.dataChuckNorris
         document.getElementById('numberFact').textContent += dataChuckNorris.length.toLocaleString()
-
-        let usedElements = [];
 
         function handleClick() {
             let randomElement;
             do {
                 const randomIndex = Math.floor(Math.random() * dataChuckNorris.length)
                 randomElement = dataChuckNorris[randomIndex]
-            } while (usedElements.includes(randomElement))
-            usedElements.push(randomElement);
+            } while (usedElements.has(randomElement))
+            usedElements.add(randomElement);
+            if (usedElements.size > 15) {
+                usedElements.delete(usedElements.values().next().value)
+            }
             bubbleContent.textContent = randomElement;
         }
 
