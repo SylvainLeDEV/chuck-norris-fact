3b072fbbc45d9f1ef51f4d6bf072c500ba2676f9
SylvainLeDev <dev.tess.sylvain@gmail.com>
2023-03-13 10:11:27 +0100

    fix bug import

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 document.getElementById('numberFact').textContent += dataChuckNorris.length.toLocaleString()
 import {dataChuckNorris} from '/assets/data/data.js'
+
 const bubble = document.getElementById('bubble')
 const bubbleContent = document.getElementById('content')
-let usedElements = [];
 
-function handleClick() {
-    let randomElement;
-    do {
-        const randomIndex = Math.floor(Math.random() * dataChuckNorris.length)
-        randomElement = dataChuckNorris[randomIndex]
-    } while (usedElements.includes(randomElement))
-    usedElements.push(randomElement);
-    bubbleContent.textContent = randomElement;
-}
+import("./assets/data/data.js")
+    .then(module => {
+        function handleClick() {
+            let usedElements = [];
+            let randomElement;
+            do {
+                const randomIndex = Math.floor(Math.random() * dataChuckNorris.length)
+                randomElement = dataChuckNorris[randomIndex]
+            } while (usedElements.includes(randomElement))
+            usedElements.push(randomElement);
+            bubbleContent.textContent = randomElement;
+        }
 
-bubble.addEventListener("click", handleClick)
+        bubble.addEventListener("click", handleClick)
+    })
+    .catch(error => {
+        console.error("Error in file dataChuckNorris : ", error)
+    });
